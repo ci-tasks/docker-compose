1e58ba3463abc75fdb9b3079725f2e2c75811a19
Svetlin Ralchev <iamralch@users.noreply.github.com>
2023-11-17 15:48:04 +0400

    Print logs before post action
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,20 @@ export async function post() {
   const docker_compose_path = core.getInput('docker-compose-path')
   const docker_compose_profile = core.getInput('docker-compose-profile')
 
-  const command = `docker-compose --file ${docker_compose_path} --profile ${docker_compose_profile} down`
+  const commands = [
+    `docker-compose --file ${docker_compose_path} --profile ${docker_compose_profile} logs`,
+    `docker-compose --file ${docker_compose_path} --profile ${docker_compose_profile} down`
+  ]
+
   const options = { cwd: process.env.GITHUB_WORKSPACE }
 
   if (fs.existsSync(docker_compose_path)) {
-    core.info(`Running ${command} ...`)
-    // execute the command
-    ps.execSync(command, options)
+    commands.forEach(command => {
+      core.info(`Running ${command} ...`)
+      // execute the command
+      const result = ps.execSync(command, options)
+      // log the result
+      core.info(result.toString())
+    })
   }
 }
